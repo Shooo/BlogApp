55deb6eacc1d78a4a27fefeb3603adeefefb67bb
sho <shomiyauchix@gmail.com>
2017-08-27 20:27:55 -0700

    Switched from semantic to bootstrap, separated routes and models

--- app.js
+++ app.js
@@ -1,128 +1,18 @@
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
-var expressSanitizer = require("express-sanitizer");
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
+var routes = require("./routes/routes.js");
+
 // APP CONFIG
-mongoose.connect("mongodb://localhost/blog_app");
+mongoose.connect("mongodb://localhost/blogapp");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(expressSanitizer());
-app.use(methodOverride("_method"));
-app.use(expressSanitizer());
 
-
-//MONGOOSE/MODEL CONFIG
-var blogSchema = new mongoose.Schema({
-	title: String,
-	image: String,
-	body: String,
-	created: {type: Date, default: Date.now}
-});
-
-var Blog = mongoose.model("Blog", blogSchema);
-
-// Blog.create({
-// 	title: "Test Blog",
-// 	image: "https://images.unsplash.com/photo-1426287658398-5a912ce1ed0a?dpr=1&auto=compress,format&crop=entropy&fit=crop&w=376&h=250&q=80&cs=tinysrgb",
-// 	body: "Hello this is a blog post"
-// });
-
-// title
-// image
-// bodyParser
-// created
-
-// RESTFUL ROUTES
-//INDEX ROUTE
-app.get("/", function(req, res){
-	res.redirect("/blogs");
-});
-
-app.get("/blogs", function(req, res){
-	Blog.find({}, function(err, blogs){
-		if(err){
-			console.log(err);
-		} else {
-			blogs.reverse();
-			res.render("index", {blogs: blogs});
-		}
-	});
-});
-
-// NEW ROUTE
-app.get("/blogs/new", function(req, res){
-	res.render("new");
-});
-
-// CREATE ROUTE
-app.post("/blogs", function(req, res){
-	//create blogs
-	// To view effects of sanitize
-	console.log(req.body);
-	//req.body.blog.body = req.sanitize(req.body.blog.body);
-	console.log("=================");
-	console.log(req.body);
-	Blog.create(req.body.blog, function(err, newBlog){
-		if(err){
-			res.render("new");
-		} else {
-			res.redirect("/blogs");
-		}
-	});
-	//redirect to index
-});
-
-// SHOW ROUTE
-app.get("/blogs/:id", function(req, res){
-	Blog.findById(req.params.id, function(err, foundBlog){
-		if(err){
-			res.redirect("/blogs");
-		} else {
-			res.render("show", {blog: foundBlog});
-		}
-	});
-});
-
-// EDIT ROUTE
-app.get("/blogs/:id/edit", function(req, res){
-	Blog.findById(req.params.id, function(err, foundBlog){
-		if(err){
-			res.redirect("/blogs");
-		} else {
-			res.render("edit", {blog: foundBlog});
-		}
-	});
-});
-
-// UPDATE ROUTE
-app.put("/blogs/:id", function(req, res){
-	//req.body.blog.body = req.sanitize(req.body.blog.body);
-	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
-		if(err){
-			res.redirect("/blogs");
-		} else {
-			res.redirect("/blogs/" + req.params.id);
-		}
-	});
-});
-
-// DELETE ROUTE
-app.delete("/blogs/:id", function(req, res){
-	//destroy blog
-	Blog.findByIdAndRemove(req.params.id, function(err){
-		if(err){
-			res.redirect("/blogs");
-		} else {
-			res.redirect("/blogs");
-		}
-	});
-	//redirect
-});
+app.use("/", routes);
 
 app.listen(3000, function(){
-	console.log("listening to port 3000");
-});
+	console.log("running...");
+})
